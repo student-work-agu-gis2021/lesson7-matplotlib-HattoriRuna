ef230a75bdd18ea71e7dd7a57a2b735a8810f739
HattoriRuna <>
2021-05-30 16:06:15 +0000

    I created a line chart. (Part 3 YOUR CODE HERE 3)

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -57,9 +57,18 @@ print("Number of rows:", len(selection))
 # 
 
 # YOUR CODE HERE 3
+# Creating a line graph
+import matplotlib.pyplot as plt
+selection = selection.sort_index()
+plt.plot(selection.index, selection['TEMP_C'], linestyle = 'solid', c = 'black', marker = 'o', markersize = 3)
+plt.title("Helsinki-Vantaa Airport")
+plt.xlabel("Time")
+plt.ylabel("Temperature(Celsius)")
+plt.grid()
+plt.show()
 
 # Set output file name
-outputfp = ""
+outputfp = "temp_line_plot.png"
 
 # Save plot as image
 # YOUR CODE HERE 4
